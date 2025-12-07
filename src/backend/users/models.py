6291b0df4a4139cd55e541d8f59f63ad175fa084
leeyun <leeyunyvn@gmail.com>
2025-12-07 13:42:52 +0900

    fix: docstring

--- src/backend/users/models.py
+++ src/backend/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.exceptions import ValidationError
 
-# === 커스텀 유저 매니저 정의 ===
 class CustomUserManager(BaseUserManager):
     def create_user(self, user_name, password=None, **extra_fields):
         if not user_name:
@@ -23,23 +22,19 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
         
-        # 커스텀 create_user 메서드를 사용하여 슈퍼유저 생성
         return self.create_user(user_name, password, **extra_fields)
 
-# === 사용자 모델 수정 ===
 class User(AbstractUser):
     """사용자 모델"""
     user_id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=150, unique=True)
     
-    # 기본 AbstractUser 필드들을 비활성화 (선택 사항)
     username = None 
     first_name = None
     last_name = None
 
-    # 커스텀 필드를 USERNAME_FIELD로 지정
     USERNAME_FIELD = 'user_name'
-    REQUIRED_FIELDS = ['email'] # 'email'을 REQUIRED_FIELDS에 추가하는 것이 일반적입니다.
+    REQUIRED_FIELDS = ['email']
 
     objects = CustomUserManager() 
     
